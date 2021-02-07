985ce0ec489792f6c0d428fd44fcec9727ded6f9
Saggitarie <saggitarie@gmail.com>
2021-02-08 01:46:06 +0900

    SCSS Loader Configuration Done

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // SCSS file in the project
+    '@/assets/styles/main.scss',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -29,7 +32,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/style-resources'],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
