9342fa637177df49abf3cec5d3d5f7660757355d
Saggitarie <saggitarie@gmail.com>
2021-02-11 14:35:29 +0900

    Slider Component Design Done

--- components/FontSizeSlider.vue
+++ components/FontSizeSlider.vue
@@ -1,9 +1,88 @@
 <template>
-  <div>FontSizeSlider</div>
+  <div>
+    <div class="slider-component">
+      <div class="slidecontainer">
+        <input
+          ref="input"
+          v-model="currentValue"
+          type="range"
+          :min="min"
+          :max="max"
+          class="slider"
+          @input="onInput"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+    min: {
+      type: Number,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    }
+  },
+  methods: {
+    onInput() {
+      // this.currentValue is a string because HTML is weird
+      this.$emit('input', parseInt(this.currentValue))
+    },
+  },
+}
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.slider-component .slidecontainer {
+  width: 100%;
+}
+
+.slider-component .slidecontainer .slider {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background: $color-navigation-hover;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  &::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 18px;
+    height: 18px;
+    background: $color-navigation-active;
+    cursor: pointer;
+    border-radius: 50%;
+  }
+
+  &::-moz-range-thumb {
+    width: 18px;
+    height: 18px;
+    background: $color-navigation-active;
+    cursor: pointer;
+    border-radius: 50%;
+  }
+}
+</style>
